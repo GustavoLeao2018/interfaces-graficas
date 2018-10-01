8fed10ed9a3d775500e0f96b18c7c4d8c077c984
GustavoLeao2018 <gustavo.leao.nogueira.2018@gmail.com>
2018-10-01 19:26:20 -0300

    Exmeplo básico criado.

---
+++ exercicio_1/Main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Exemplo básico de interface gráfica."""
+
+from tkinter import *
+
+
+# Cria a instância de tkinter
+app = Tk()
+
+# Colocando um título
+app.title("Este é um titulo!")
+
+# Colocando um tamanho
+app.geometry("800x600")
+
+# Inicia o loop
+app.mainloop()
+
